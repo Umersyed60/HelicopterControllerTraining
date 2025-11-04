256f8025967d8956b95c8a9962a56594657207a1
umer-aftab <umersyed2014@gmail.com>
2025-11-05 01:22:12 +0500

    Stub in camera scripts
    Stubing in camera scripts creating base of heli camera.

---
+++ Assets/Intro_Heli_Physics/Code/my_Scripts/Scripts/Camera/my_Heli_Camera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace My_Practice
+{
+    public class my_Heli_Camera : MonoBehaviour, my_IHeliCamera
+    {
+        #region Variables
+        [Header("Camera Properties")]
+        public Rigidbody rb;
+        public float height = 2f;
+        public float distance = 2f;
+        #endregion
+
+        #region Builtin Methods
+        private void FixedUpdate()
+        {
+            if (rb)
+            {
+                UpdateCamera();
+            }
+        }
+        #endregion
+
+        #region Interface Methods
+        public void UpdateCamera()
+        {
+            Debug.Log("Camera is Updating");
+        }
+        #endregion
+    }
+
+}
