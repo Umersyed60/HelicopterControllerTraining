e5fb1dc640509d63500b2d9869548f1b1e46a035
umer-aftab <umersyed2014@gmail.com>
2025-11-11 02:24:21 +0500

    Heli min max boundary visualization
    Heli min max boundary visualization added through editor script.

---
+++ Assets/Intro_Heli_Physics/Code/my_Scripts/Scripts/Camera/Editor/my_AdvancedHeliCamera_Editor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace My_Practice
+{
+    [CustomEditor(typeof(my_Advanced_HeliCamera))]
+    public class my_AdvancedHeliCamera_Editor : Editor
+    {
+        #region Variables
+        my_Advanced_HeliCamera targetCamera;
+        #endregion
+
+        #region Methods
+        private void OnEnable()
+        {
+            targetCamera = (my_Advanced_HeliCamera)target;
+        }
+
+        private void OnSceneGUI()
+        {
+            float minDist = targetCamera.minDistance;
+            float maxDist = targetCamera.maxDistance;
+            Vector3 targetFwd = targetCamera.rb.transform.forward;
+
+            Handles.color = Color.blue;
+            Handles.DrawWireDisc(targetCamera.rb.position, Vector3.up, minDist);
+            Handles.DrawWireDisc(targetCamera.rb.position, Vector3.up, maxDist);
+
+            targetCamera.minDistance = Handles.ScaleSlider(targetCamera.minDistance, targetCamera.rb.position + (targetFwd * minDist), Vector3.forward, Quaternion.identity, 1f, 1f);
+            targetCamera.maxDistance = Handles.ScaleSlider(targetCamera.maxDistance, targetCamera.rb.position + (targetFwd * maxDist), Vector3.forward, Quaternion.identity, 1f, 1f);
+
+        }
+        #endregion
+    }
+}
