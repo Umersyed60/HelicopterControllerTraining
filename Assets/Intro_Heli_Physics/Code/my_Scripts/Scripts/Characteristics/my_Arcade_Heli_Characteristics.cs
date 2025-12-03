893258aaf58042b9d3ce98f469e8525ab3d70b78
umer-aftab <umersyed2014@gmail.com>
2025-12-03 17:19:28 +0500

    Added arcade style heli pedal rotation
    Added arcade style heli pedal rotation

--- Assets/Intro_Heli_Physics/Code/my_Scripts/Scripts/Characteristics/my_Arcade_Heli_Characteristics.cs
+++ Assets/Intro_Heli_Physics/Code/my_Scripts/Scripts/Characteristics/my_Arcade_Heli_Characteristics.cs
@@ -6,6 +6,12 @@ namespace My_Practice
 {
     public class my_Arcade_Heli_Characteristics : my_Heli_Characteristics
     {
+        #region Variables
+        private float yRot = 0f;
+        private float xRot = 0f;
+        private float zRot = 0f;
+        #endregion
+
         protected override void HandleLift(Rigidbody _rb, my_Input_Controller input)
         {
             //base.HandleLift(_rb, input);
@@ -28,6 +34,10 @@ namespace My_Practice
         protected override void HandlePedals(Rigidbody _rb, my_Input_Controller input)
         {
             //base.HandlePedals(_rb, input);
+            yRot += input.PedalInput * tailForce;
+
+            Quaternion wantedRot = Quaternion.Euler(0f, yRot, 0f);
+            _rb.MoveRotation(wantedRot);
         }
     }
 }
