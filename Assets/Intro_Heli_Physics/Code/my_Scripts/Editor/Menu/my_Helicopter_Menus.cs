61e8d338aa745e9031ebd3981a0e4065e70eee2e
umer-aftab <umersyed2014@gmail.com>
2025-12-03 15:54:08 +0500

    Basic arcade heli scene component setup
    Basic arcade heli scene component setup

--- Assets/Intro_Heli_Physics/Code/my_Scripts/Editor/Menu/my_Helicopter_Menus.cs
+++ Assets/Intro_Heli_Physics/Code/my_Scripts/Editor/Menu/my_Helicopter_Menus.cs
@@ -25,13 +25,42 @@ namespace My_Practice
             GameObject audioGRP = new GameObject("Audio_GRP");
             GameObject graphicsGRP = new GameObject("Graphics_GRP");
             GameObject colGRP = new GameObject("Collision_GRP");
+            GameObject engineGRP = new GameObject("Engine_GRP");
+            SetupEngineGRP(engineGRP, newHeliController);
+            GameObject rotorGRP = new GameObject("Rotor_GRP");
+            SetupRotorGRP(rotorGRP, newHeliController);
 
             audioGRP.transform.SetParent(newHeli.transform);
             graphicsGRP.transform.SetParent(newHeli.transform);
             colGRP.transform.SetParent(newHeli.transform);
-            
+            engineGRP.transform.SetParent(newHeli.transform);
+            rotorGRP.transform.SetParent(newHeli.transform);
+
             //Select newHeli in Hierarchy
             Selection.activeGameObject = newHeli;
+        }
+
+        static void SetupRotorGRP(GameObject rotorgo, my_Heli_Controller controller)
+        {
+            my_Heli_Rotor_Controller rotorController = rotorgo.AddComponent<my_Heli_Rotor_Controller>();
+            controller.rotorCtrl = rotorController;
+
+            GameObject mainGRP = new GameObject("Main_Rotor");
+            mainGRP.AddComponent<my_HeliMain_Rotor>();
+            GameObject tailGRP = new GameObject("Tail_Rotor");
+            tailGRP.AddComponent<my_HeliTail_Rotor>();
+
+            mainGRP.transform.SetParent(rotorgo.transform);
+            tailGRP.transform.SetParent(rotorgo.transform);
+        }
+
+        static void SetupEngineGRP(GameObject enginego, my_Heli_Controller controller)
+        {
+            GameObject engineGRP = new GameObject("Main_Engine");
+            my_Heli_Engine engine = engineGRP.AddComponent<my_Heli_Engine>();
+            controller.engines.Add(engine);
+
+            engineGRP.transform.SetParent(enginego.transform);
         }
     }
 }
