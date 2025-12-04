5d18e4a7ce8b78b2a2172cb5ab43848d880de9da
umer-aftab <umersyed2014@gmail.com>
2025-12-05 01:36:09 +0500

    Added gatling gun rotation
    Added gatling gun rotation on firing

--- Assets/Intro_Heli_Physics/Code/my_Scripts/Scripts/Weapons/Weapon_Types/my_GatlingGun_Weapon.cs
+++ Assets/Intro_Heli_Physics/Code/my_Scripts/Scripts/Weapons/Weapon_Types/my_GatlingGun_Weapon.cs
@@ -10,6 +10,21 @@ namespace My_Practice
         [Header("Gatling Gun Properties")]
         public Transform barrelGO;
         public float rotationSpeed = 10f;
+        public float slowDownSpeed = 2f;
+
+        private float lastRotSpeed = 0f;
+        #endregion
+
+        #region BuiltIn Methods
+        private void Update()
+        {
+            if (barrelGO)
+            {
+                lastRotSpeed -= Time.deltaTime * slowDownSpeed;
+                lastRotSpeed = Mathf.Clamp(lastRotSpeed, 0f, rotationSpeed);
+                barrelGO.Rotate(Vector3.up, lastRotSpeed);
+            }
+        }
         #endregion
 
         #region Override Methods
@@ -18,7 +33,11 @@ namespace My_Practice
             base.FireWeapon();
 
             //Animate the Barrels
-
+            if (barrelGO)
+            {
+                barrelGO.Rotate(Vector3.up, rotationSpeed);
+                lastRotSpeed = rotationSpeed;
+            }
         }
         #endregion
     }
