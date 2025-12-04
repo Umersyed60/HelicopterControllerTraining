2f00569ce7a8168fc1f1b2b3967500054ae35cbb
umer-aftab <umersyed2014@gmail.com>
2025-12-05 01:25:22 +0500

    Gatling gun weapon initial setup
    Gatling gun weapon initial setup

---
+++ Assets/Intro_Heli_Physics/Code/my_Scripts/Scripts/Weapons/Weapon_Types/my_GatlingGun_Weapon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace My_Practice
+{
+    public class my_GatlingGun_Weapon : my_RapidFire_Weapon
+    {
+        #region Variables
+        [Header("Gatling Gun Properties")]
+        public Transform barrelGO;
+        public float rotationSpeed = 10f;
+        #endregion
+
+        #region Override Methods
+        public override void FireWeapon()
+        {
+            base.FireWeapon();
+
+            //Animate the Barrels
+
+        }
+        #endregion
+    }
+}
