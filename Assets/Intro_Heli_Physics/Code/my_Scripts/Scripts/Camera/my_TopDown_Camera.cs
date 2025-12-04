8fc33d30946dfa8d0487cf426d96a2fa0ceae8c3
umer-aftab <umersyed2014@gmail.com>
2025-12-04 18:08:42 +0500

    Basic top down camera scene setup
    Basic top down camera scene and script setup and quality settings updated

---
+++ Assets/Intro_Heli_Physics/Code/my_Scripts/Scripts/Camera/my_TopDown_Camera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace My_Practice
+{
+    public class my_TopDown_Camera : my_Base_HeliCamera, my_IHeliCamera
+    {
+        #region Variables
+        [Header("Top Down Camera Properties")]
+        public float height = 2f;
+        public float distance = 2f;
+        #endregion
+
+        #region Builtin Methods
+        private void OnEnable()
+        {
+            updateEvent.AddListener(UpdateCamera);
+        }
+
+        private void OnDisable()
+        {
+            updateEvent.RemoveListener(UpdateCamera);
+        }
+        #endregion
+
+        #region Interface Methods
+        public void UpdateCamera()
+        {
+
+        }
+        #endregion
+    }
+}
